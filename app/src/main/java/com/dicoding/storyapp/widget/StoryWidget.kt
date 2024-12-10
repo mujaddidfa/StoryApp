e0cbdb83e9560097740f8308a7e49c327b3e51fb
Mujaddid Fathi Athoillah <mujaddid201@gmail.com>
2024-12-10 11:41:26 +0700

    fix widget bug

--- app/src/main/java/com/dicoding/storyapp/widget/StoryWidget.kt
+++ app/src/main/java/com/dicoding/storyapp/widget/StoryWidget.kt
@@ -7,15 +7,30 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.widget.RemoteViews
-import android.widget.Toast
 import androidx.core.net.toUri
 import com.dicoding.storyapp.R
+import com.dicoding.storyapp.view.main.MainActivity
 
 class StoryWidget : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if (intent.action != null) {
+            if (intent.action == OPEN_APP_ACTION) {
+                val launchIntent = Intent(context, MainActivity::class.java)
+                launchIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                context.startActivity(launchIntent)
+            }
+        }
+    }
 
     companion object {
-
-        private const val TOAST_ACTION = "com.dicoding.picodiploma.TOAST_ACTION"
+        private const val OPEN_APP_ACTION = "com.dicoding.picodiploma.OPEN_APP_ACTION"
         const val EXTRA_ITEM = "com.dicoding.picodiploma.EXTRA_ITEM"
 
         @Suppress("DEPRECATION")
@@ -28,35 +43,18 @@ class StoryWidget : AppWidgetProvider() {
             views.setRemoteAdapter(R.id.stack_view, intent)
             views.setEmptyView(R.id.stack_view, R.id.empty_view)
 
-            val toastIntent = Intent(context, StoryWidget::class.java)
-            toastIntent.action = TOAST_ACTION
-            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-
-            val toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
+            val openAppIntent = Intent(context, StoryWidget::class.java).apply {
+                action = OPEN_APP_ACTION
+            }
+            val openAppPendingIntent = PendingIntent.getBroadcast(context, 0, openAppIntent,
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
                     PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
                 else 0
             )
-
-            views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
+            views.setOnClickPendingIntent(R.id.widget_layout, openAppPendingIntent)
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
 
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
-        }
-    }
-
-    override fun onReceive(context: Context, intent: Intent) {
-        super.onReceive(context, intent)
-        if (intent.action != null) {
-            if (intent.action == TOAST_ACTION) {
-                val viewIndex = intent.getIntExtra(EXTRA_ITEM, 0)
-                Toast.makeText(context, "Touched view $viewIndex", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
 }
